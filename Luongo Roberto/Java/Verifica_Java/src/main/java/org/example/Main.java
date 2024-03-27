b07de270c1f3a859d84417317b44c9b0167af3d2
Dokjolly0 <alexviolattolibero.it@gmail.com>
2024-03-27 10:54:46 +0100

    Verifica_Java_Menu

--- Luongo Roberto/Java/Verifica_Java/src/main/java/org/example/Main.java
+++ Luongo Roberto/Java/Verifica_Java/src/main/java/org/example/Main.java
@@ -1,7 +1,45 @@
 package org.example;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        int numero = 1;
+        while(numero >= 1 && numero <= 5)
+        {
+            System.out.println("Inserisci un numero da 1 a 5:");
+            System.out.println("1. Inserimento nuovo ordine");
+            System.out.println("2. Calcola fabbisopgni per un ordine");
+            System.out.println("3. Visualizza fabbisogni per un ordine");
+            System.out.println("4. Scarico magazzino");
+            System.out.println("5. Esci dal menu");
+
+            numero = scanner.nextInt();
+
+            if(numero == 1)
+            {
+//                System.out.println("Inserisci un nuovo ordine");
+            }
+            else if(numero == 2)
+            {
+//                System.out.println("Calcola fabbisogni per un ordine");
+            }
+            else if(numero == 3)
+            {
+//                System.out.println("Visualizza fabbisogni per un ordine");
+            }
+            else if(numero == 4)
+            {
+//                System.out.println("Scarico magazzino");
+            }
+            else if(numero == 5)
+            {
+                return;
+            }
+            else
+            {
+                System.out.println("Inserisci un numero da 1 a 5");
+            }
+        }
     }
 }
