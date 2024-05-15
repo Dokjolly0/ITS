f8e9beb7367388322401f4b092ac9bb0f4df2c47
Dokjolly0 <alexviolattolibero.it@gmail.com>
2024-05-15 15:07:10 +0200

    Add change date

--- Battistella_Enrico/Backand/remake_cart/src/api/user/user.controller.ts
+++
@@ -1,6 +0,0 @@
-import { NextFunction, Response } from "express";
-import { TypedRequest } from "../../utils/typed-request";
-
-export const me = async (req: TypedRequest, res: Response, next: NextFunction) => {
-  res.json(req.user!);
-}
--- Battistella_Enrico/Backand/remake_cart/src/api/user/user.router.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { isAuthenticated } from '../../utils/auth/authenticated-middleware';
-import { me } from './user.controller';
-
-const router = express.Router();
-
-router.get('/me', isAuthenticated, me);
-
-export default router;
